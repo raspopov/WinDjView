f7d255df1bfa7f3d6b2bf9d57f42b946b2a64d7d
zhezherun <>
2006-01-13 13:08:38 +0000

    *** empty log message ***

--- windjview/GotoPageDlg.cpp
+++ windjview/GotoPageDlg.cpp
@@ -16,7 +16,7 @@
 //	Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 //	http://www.gnu.org/copyleft/gpl.html
 
-// $Id $
+// $Id$
 
 #include "stdafx.h"
 #include "WinDjView.h"
